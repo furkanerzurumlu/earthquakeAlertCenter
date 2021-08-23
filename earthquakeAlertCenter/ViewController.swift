f507e9a278dc30919d847c5e305f3483738c4784
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2021-08-23 19:21:52 +0300

    Initial Commit

---
+++ earthquakeAlertCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  earthquakeAlertCenter
+//
+//  Created by Furkan Erzurumlu on 23.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
