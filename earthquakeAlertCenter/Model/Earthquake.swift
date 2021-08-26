8d87c287f85fefca221686fe681370efb77d0b78
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2021-08-26 14:51:28 +0300

    Model Commit Add

--- earthquakeAlertCenter/Model/Earthquake.swift
+++ earthquakeAlertCenter/Model/Earthquake.swift
@@ -7,18 +7,14 @@
 
 import Foundation
 
-// MARK: - Welcome
-/*struct Welcome: Codable {
-    let success: Bool
-    let data: [Datum]
+struct Earthquake : Decodable {
+    let success : Bool
+    let data : [Datum]
 }
 
-// MARK: - Datum
-struct Datum: Codable {
-    let date: String
-    let latitude, longitude, depth: Double
-    let md: Md
-    let ml, mw, place: String
-    let status: Status
+struct Datum : Decodable {
+    let date : String
+    let latitude, longitude, depth : Double
+    let md, ml, mw, place : String
+    let status : String
 }
- */
